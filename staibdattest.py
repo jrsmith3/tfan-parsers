7feb3dcb2d113d5379dd566795f121d11dc01ccb
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2010-03-10 23:00:01 -0500

    Partially updated the test suite.

--- staibdattest.py
+++ staibdattest.py
@@ -27,6 +27,7 @@ from StaibDat import FormatError
 import unittest
 import random
 import os
+from numpy import *
 
 class InvalidDataFile(unittest.TestCase):
   """
@@ -167,6 +168,111 @@ class CorrectlyImportedData(unittest.TestCase):
   Tests to see if the resulting object has the correct data.
   """
   pass
+  
+class APITest(unittest.TestCase):
+  """
+  Tests to see if the API is correct.
+  """
+  
+  filename = "testfiles/good_data.dat"
+  
+  def testStaibDatfilenameExist(self):
+    """filename key should exist."""
+    pass
+    
+  def testStaibDatfilenameStr(self):
+    """filename data should be a string."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["filename"]),type(str()))
+    
+  def testStaibDatfilenameVal(self):
+    """filename data should match instantiating string."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(SD["filename"],self.filename)
+    
+  def testStaibDatfileTextExist(self):
+    """fileText key should exist."""
+    pass
+    
+  def testStaibDatfileTextStr(self):
+    """fileText data should be a string."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["fileTest"]),type(str()))
+    
+  def testStaibDatfileTextVal(self):
+    """fileText data should match the data in the file."""
+    pass
+    
+  def testStaibDatKEExist(self):
+    """KE key should exist."""
+    pass
+    
+  def testStaibDatKEArray(self):
+    """KE data should be a numpy array."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["KE"]),ndarray)
+    
+  def testStaibDatKESize(self):
+    """KE array should be the correct size."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(SD["KE"].shape[0],SD["DataPoints"])
+    
+  def testStaibDatKEValues(self):
+    """KE array should have the proper values."""
 
+  def testStaibDatBEExist(self):
+    """BE key should exist."""
+    pass
+    
+  def testStaibDatBEArray(self):
+    """BE data should be a numpy array."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["BE"]),ndarray)
+    
+  def testStaibDatBESize(self):
+    """BE array should be the correct size."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(SD["BE"].shape[0],SD["DataPoints"])
+    
+  def testStaibDatBEValues(self):
+    """BE array should have the proper values."""
+    pass
+  
+  def testStaibDatC1Exist(self):
+    """C1 key should exist."""
+    pass
+    
+  def testStaibDatC1Array(self):
+    """C1 data should be a numpy array."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["C1"]),ndarray)
+    
+  def testStaibDatC1Size(self):
+    """C1 array should be the correct size."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(SD["C1"].shape[0],SD["DataPoints"])
+    
+  def testStaibDatC1Values(self):
+    """C1 array should have the proper values."""
+    pass
+    
+  def testStaibDatC2Exist(self):
+    """C2 key should exist."""
+    pass
+    
+  def testStaibDatC2Array(self):
+    """C2 data should be a numpy array."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(type(SD["C2"]),ndarray)
+    
+  def testStaibDatC2Size(self):
+    """C2 array should be the correct size."""
+    SD = StaibDat(self.filename)
+    self.assertEqual(SD["C2"].shape[0],SD["DataPoints"])
+    
+  def testStaibDatC2Values(self):
+    """C2 array should have the proper values."""
+    pass
+  
 if __name__ == '__main__':
   unittest.main()

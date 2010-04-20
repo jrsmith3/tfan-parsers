292817cb07f57a34af3b1fb027cf5011f62c3926
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2010-04-19 20:12:55 -0400

    Added setup file and reorganized the project to be a package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='tfan_parsers',
+      version='02',
+      author='Joshua Ryan Smith',
+      author_email='joshua.r.smith@gmail.com',
+      url='http://github.com/jrsmith3/tfan-parsers',
+      description='Imports data from TFAN lab as dictionary type objects.',
+      license='GPL',
+      packages=['tfan_parsers'],
+     )
